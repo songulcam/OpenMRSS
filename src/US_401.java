c4b841ea544164fc34dcd324af19875cef0321b5
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-10-06 05:33:22 +0300

    değişiklik gönderiliyor.

--- src/US_401.java
+++ src/US_401.java
@@ -13,12 +13,12 @@ public class US_401 extends BaseDriver {
     public void checkingLoginErrorsSystem(String username, String password, boolean expectedResult) {
         US_401_404_407_POM locater = new US_401_404_407_POM();
 
-        for (int i = 0; i <2; i++) {
+        for (int i = 0; i < 2; i++) {
             wait.until(ExpectedConditions.elementToBeClickable(locater.languageButton));
             locater.languageButton.click();
             wait.until(ExpectedConditions.visibilityOf(locater.languageEnglish));
             locater.languageEnglish.click();
-         }
+        }
 
         wait.until(ExpectedConditions.elementToBeClickable(locater.demoButton));
         locater.demoButton.click();
@@ -29,29 +29,25 @@ public class US_401 extends BaseDriver {
         wait.until(ExpectedConditions.elementToBeClickable(locater.enterOpenMrs2));
         locater.enterOpenMrs2.click();
 
+        wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("LoginURL")));
+        locater.userName.sendKeys(username);
+        locater.password.sendKeys(password);
+        locater.loginButton.click();
+        MyFunc.Wait(1);
 
-        if (expectedResult==false){
-            wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("LoginURL")));
-            locater.userName.sendKeys(username);
-            locater.password.sendKeys(password);
-            locater.loginButton.click();
-            MyFunc.Wait(1);
+        if (!expectedResult) {
             Assert.assertTrue(locater.locationError.getText().contains("You must choose "));
         } else {
-            wait.until(ExpectedConditions.urlToBe(ConfigReader.getProperty("LoginURL")));
-            locater.userName.sendKeys(username);
-            locater.password.sendKeys(password);
-            locater.inpatientWard.click();
-            locater.loginButton.click();
             Assert.assertTrue(locater.loginControl.getText().contains("Logged in as Super User "));
         }
     }
+
     @DataProvider
-    Object[][] userData(){
-        Object[][] userPassword={
-                {"Ümmühan", "123457a",false},
+    public Object[][] userData() {
+        Object[][] userPassword = {
+                {"Ümmühan", "123457a", false},
                 {"Merve", "12dd7a", false},
-                {"Sefa","1vvv57a", false},
+                {"Sefa", "1vvv57a", false},
                 {"Kerim", "1xxx7a", false},
                 {"Songül", "1abc7a", false},
                 {"admin", "Admin123", true},
